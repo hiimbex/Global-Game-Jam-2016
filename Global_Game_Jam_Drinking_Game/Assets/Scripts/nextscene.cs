f60d28b6de90457b4b1a58193272d0e439b08022
Bex Warner <hiimbex@stuarts-mbp-3.american.edu>
2016-01-30 16:37:38 -0500

    more work on plane and buttons for android

---
+++ Global_Game_Jam_Drinking_Game/Assets/Scripts/nextscene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextscene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Forward")) {
+			Debug.Log ("button pressed");
+			Application.LoadLevel ("USA");
+
+		}
+	}
+}
