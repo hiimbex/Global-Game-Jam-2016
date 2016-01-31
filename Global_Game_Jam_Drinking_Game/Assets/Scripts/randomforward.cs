7c85f244dea8c3c7da4d441d77cd9ea6f65f4373
SOC <soc@mck-t13-8.american.edu>
2016-01-31 13:11:35 -0500

    buttone

--- Global_Game_Jam_Drinking_Game/Assets/Scripts/randomforward.cs
+++ Global_Game_Jam_Drinking_Game/Assets/Scripts/randomforward.cs
@@ -6,10 +6,26 @@ public class randomforward : MonoBehaviour {
 
 	public void randomGoForward(){
 		Debug.Log ("random!");
-		float rand= Mathf.Floor(Random.Range(1,3));
+		float rand= Mathf.Floor(Random.Range(1,4));
 		string level = rand.ToString ();
 		Debug.Log (level);
 		SceneManager.LoadScene (level,LoadSceneMode.Single);
 	}
 
+	public void toBeerPong(){
+		SceneManager.LoadScene ("BeerPong");
+	}
+
+	public void toKottabos(){
+		SceneManager.LoadScene ("Kottabos");
+	}
+
+	public void toCredits(){
+		SceneManager.LoadScene ("Credits");
+	}
+		
+
+	public void toRussianRoulette(){
+		SceneManager.LoadScene ("Russian Roulette");
+	}
 }
