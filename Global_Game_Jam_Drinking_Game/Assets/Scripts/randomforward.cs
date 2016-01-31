04458aae14ef23527bf40f9371c04b26c986d754
SOC <soc@mck-t13-8.american.edu>
2016-01-31 12:03:27 -0500

    random forward

---
+++ Global_Game_Jam_Drinking_Game/Assets/Scripts/randomforward.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class randomforward : MonoBehaviour {
+
+	public void randomGoForward(){
+		Debug.Log ("random!");
+		float rand= Mathf.Floor(Random.Range(1,2));
+		string level = rand.ToString ();
+		Debug.Log (level);
+		SceneManager.LoadScene (level);
+	}
+
+}
