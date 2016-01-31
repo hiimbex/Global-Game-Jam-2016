0f8c46c2c506ccfa95271ec59d84d3fc807fccb5
Bex Warner <hiimbex@stuarts-mbp-3.american.edu>
2016-01-31 11:33:59 -0500

    adding greece

--- Global_Game_Jam_Drinking_Game/Assets/Scripts/onClickForward.cs
+++ Global_Game_Jam_Drinking_Game/Assets/Scripts/onClickForward.cs
@@ -4,19 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class onClickForward : MonoBehaviour {
 
-	public void nextScene(){
+	public void gotoUSA(){
 		// Save game data
 	
 		// Close game
 		Debug.Log ("CLICKING");
 		SceneManager.LoadScene ("USA");
 	}
-	public void credits(){
-		// Save game data
 
-		// Close game
-		Debug.Log ("CLICKING");
-		SceneManager.LoadScene ("_Scenes/Credits");
-	}
 
 }
