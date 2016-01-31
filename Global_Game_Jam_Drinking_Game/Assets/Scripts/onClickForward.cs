da6e17513bee29708de6373db61fd82a7ffe7524
Bex Warner <hiimbex@stuarts-mbp-3.american.edu>
2016-01-30 20:10:59 -0500

    nav bar imporvements

---
+++ Global_Game_Jam_Drinking_Game/Assets/Scripts/onClickForward.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class onClickForward : MonoBehaviour {
+
+	public void nextScene(){
+		// Save game data
+	
+		// Close game
+		Debug.Log ("CLICKING");
+		SceneManager.LoadScene ("_Scenes/USA");
+	}
+}
